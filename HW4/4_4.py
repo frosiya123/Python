ef153900c0a553498a8c49019d0e2c7233bfe403
sweeties <frosiya@mail.ru>
2022-09-16 13:10:48 +0300

    Задача 4

---
+++ HW4/4_4.py
@@ -0,0 +1,21 @@
+# Задана натуральная степень k. Сформировать случайным образом 
+# список коэффициентов  (значения от 0 до 100) многочлена 
+# и записать в файл многочлен степени k.
+# Пример:
+
+#     k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+import random
+
+k = int(input('Введите натуральную степень числа: '))
+
+sstr = ''
+while k > 0:
+    sstr += str(random.randint(0,100)) + 'x' + '^' + str(k) + ' + '
+    k -= 1
+
+sstr += str(random.randint(0,100)) + ' = 0'
+
+#print(sstr)
+data = open('fail1.txt', 'a')
+data.writelines(sstr)
+data.close
